60d19497b3b666a781f40520238270934e6b16d2
jinxdash <jinxdash.github@gmail.com>
2023-05-23 15:25:32 +0200

    fix(22): avoid removing optional semi in rare rust compiler bug

---
+++ tests/samples/issues/22.rs
@@ -0,0 +1,67 @@
+fn preserve_last_semicolon() {
+	if let Some(left) = node.borrow().left.as_ref() {
+		deque.push_back(left.clone());
+	};
+	if let Some(right) = node.borrow().right.as_ref() {
+		deque.push_back(right.clone());
+	};
+}
+
+fn a() {
+    if let Ok(_) = lock.try_lock() {};
+}
+
+fn b() {
+    let lock = std::sync::Mutex::new(10);
+    match lock.try_lock() {
+		Ok(_) => {}
+		Err(_) => {}
+	};
+    match lock.try_lock() {
+		Ok(_) => {}
+		Err(_) => {}
+	};
+}
+
+fn c() {
+    if let Ok(_) = lock.try_lock() {};
+	// comment
+}
+
+fn d() {
+    if let Ok(_) = lock.try_lock() {}; // comment
+}
+
+fn e() {
+    if let Ok(_) = lock.try_lock() {}/** comment */;
+}
+
+fn f() {
+    if let Ok(_) = lock.try_lock() {
+
+	}
+	;
+}
+
+fn g() {
+    if let Ok(_) = lock.try_lock() {
+		
+	}
+	// comment
+	;
+}
+
+fn h() {
+	if let Ok(_) = lock {};
+	if let Ok(_) = lock {};
+}
+
+fn i() {
+	match lock {};
+	match lock {};
+}
+
+fn inner_attr() {
+    if let Ok(_) = lock.try_lock() {};
+	#![attr]
+}
